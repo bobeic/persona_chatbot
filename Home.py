567e045726d5456897911e21b461a7a8ad0e065b
Toby <toby_van_mook@hotmail.com>
2023-07-30 11:02:29 +0100

    Added Add_Docs page and fixed api key issue

--- Home.py
+++ Home.py
@@ -1,34 +1,11 @@
-from langchain.document_loaders import YoutubeLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from enum import Enum
 import streamlit as st
-from PyPDF2 import PdfReader
-from io import StringIO
-import pickle
-import os
-
-# setup doc_type enum
-DocType = Enum("DocType", ["text/csv", "youtube", "pdf"])
-
-store_path = "faiss_store_persona.pk1"
 
 
-def load_vector_store():
-    with open(store_path, "rb") as f:
-        db = pickle.load(f)
-
-    st.success("Successfully loaded store")
-    st.session_state.db = db
-
-    return db
-
-
-# streamlit website
 def main():
-    st.set_page_config(page_title="Custom chatbot 💬")
-    st.title("Add docs")
+    st.title("Home")
+    st.write(
+        "Welcome to Custom Chatbot where you can create a personalised chatbot by providing documents for its knowledge base!"
+    )
 
     with st.sidebar:
         api_key = st.text_input("OpenAI api key", type="password")
@@ -41,90 +18,6 @@ def main():
             st.error("Please enter your api key")
         else:
             st.session_state.api_key = api_key
-            embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-
-    # embeddings = OpenAIEmbeddings()
-
-    current_type = st.selectbox("type", [doc.name for doc in DocType])
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=50,
-        length_function=len,
-    )
-
-    texts_list = []
-    docs_list = []
-
-    if current_type == "text/csv":
-        file = st.file_uploader("Upload text file here")
-        if file:
-            stringio = StringIO(file.getvalue().decode("utf-8"))
-
-            texts = text_splitter.split_text(stringio.read())
-            st.write("Loaded file")
-            texts_list += texts
-
-    elif current_type == "youtube":
-        url = st.text_input("Youtube url")
-        if url:
-            loader = YoutubeLoader.from_youtube_url(url)
-            transcript = loader.load()
-            docs = text_splitter.split_documents(transcript)
-            st.write("Loaded file")
-            docs_list += docs
-
-    elif current_type == "pdf":
-        pdf = st.file_uploader("Upload pdf file here", type="pdf")
-        if pdf:
-            pdf_reader = PdfReader(pdf)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-            texts = text_splitter.split_text(text)
-            st.write("Loaded file")
-            texts_list += texts
-
-    else:
-        raise Exception("Not a valid form of document")
-
-    col1, col2, _ = st.columns([1, 1, 3])
-
-    with col1:
-        if st.button("Submit docs"):
-            # print(f"docs: {docs_list}")
-            # print(f"texts: {texts_list}")
-            if docs_list and texts_list:
-                print("docs and texts")
-                db1 = FAISS.from_texts(texts_list, embeddings)
-                db.merge_from(db1)
-
-            elif docs_list:
-                print("docs")
-                db = FAISS.from_documents(docs_list, embeddings)
-
-            elif texts_list:
-                print("texts")
-                db = FAISS.from_texts(texts_list, embeddings)
-
-            else:
-                st.write("No docs submitted")
-
-            with open(store_path, "wb") as f:
-                pickle.dump(db, f)
-
-            # st.write("successfully stored docs")
-
-            st.session_state.db = db
-
-            # if "db" not in st.session_state.keys():
-            #     st.write("No db stored")
-            # else:
-            #     st.write("db stored")
-
-    with col2:
-        if os.path.isfile(store_path):
-            st.button("Load docs", on_click=load_vector_store)
 
 
 if __name__ == "__main__":
--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -22,70 +22,70 @@ def search_docs(query):
     return docs_content
 
 
-def query_llm(query):
+def query_llm(query, chain):
     docs = search_docs(query)
     response = chain.run(question=query, docs=docs)
     response = response.replace("\n", "")
     return response
 
+def load_page():
+    # setup llm
+    # initially don't pass in previous messages
 
-# setup llm
-# initially don't pass in previous messages
+    system_template = """
+        You are a helpful assistant that can answer questions about the documents provided: {docs}
 
-system_template = """
-    You are a helpful assistant that can answer questions about the documents provided: {docs}
+        Only use the factual information from the transcript to answer the question.
 
-    Only use the factual information from the transcript to answer the question.
+        If you feel like you don't have enough information, say "I don't know".
 
-    If you feel like you don't have enough information, say "I don't know".
+        Your answers should be verbose and detailed.
+    """
 
-    Your answers should be verbose and detailed.
-"""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
 
-system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+    human_template = "Answer the following question: {question}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
-human_template = "Answer the following question: {question}"
-human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+    chat = ChatOpenAI(
+            model="gpt-3.5-turbo", openai_api_key=st.session_state.api_key
+        )
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
 
-chat_prompt = ChatPromptTemplate.from_messages(
-    [system_message_prompt, human_message_prompt]
-)
+    # app layout
+    st.title("Custom chatbot 💬")
+
+    # with st.chat_message("user"):
+    #     st.write("Hello 👋")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # syntax checks input is not empty and assigns it
+    if prompt := st.chat_input("Say something"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            response = query_llm(prompt, chain)
+            message_placeholder.markdown(response)
+
+
+
+
 
 if "api_key" not in st.session_state.keys():
     st.error("No api key")
 else:
-    # st.write(st.session_state.api_key)
-    try:
-        chat = ChatOpenAI(
-            model="gpt-3.5-turbo", openai_api_key=st.session_state.api_key
-        )
-        chain = LLMChain(llm=chat, prompt=chat_prompt)
-
-        # app layout
-        st.title("Custom chatbot 💬")
-
-        # with st.chat_message("user"):
-        #     st.write("Hello 👋")
-
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
-
-        # syntax checks input is not empty and assigns it
-        if prompt := st.chat_input("Say something"):
-            with st.chat_message("user"):
-                st.markdown(prompt)
-
-            st.session_state.messages.append({"role": "user", "content": prompt})
-
-            with st.chat_message("assistant"):
-                message_placeholder = st.empty()
-                response = query_llm(prompt)
-                message_placeholder.markdown(response)
-
-    except:
-        st.error("Invalid api key")
+    load_page()
